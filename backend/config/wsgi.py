4aa13c6b2deabf7b163b30c859aa63665b801304
ravikishorereddy9563-tech <ravikishorereddy9563@gmail.com>
2026-08-23 18:18:17 +0530

    Fix Django Vercel configuration

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,16 +1,6 @@
-"""
-WSGI config for config project.
-"""
-
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-if BASE_DIR not in sys.path:
-    sys.path.insert(0, BASE_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
